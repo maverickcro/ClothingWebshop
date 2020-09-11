a0057ae8e9d3b1a8633ff5d0cbd7c817a8bee931
maverickcro <mav3ricktv@gmail.com>
2020-09-11 18:05:50 +0200

    updated menu-item component to navigate withRouterHOC. Defined routes for homepage and hatsPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from '../src/pages/Homepage/homepage.component';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return(
+    <div>
+      <h2>Hats Page</h2>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
