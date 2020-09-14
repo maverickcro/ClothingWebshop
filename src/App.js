dd2735f2fdb35e8fd474d95bf6e73f753c79d07f
maverickcro <mav3ricktv@gmail.com>
2020-09-14 12:57:53 +0200

    finished sign in page and a custom button component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
+
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,6 +18,9 @@ const Header = () => (
             <Link className='option'to='shop'>
                 CONTACT
             </Link>
+            <Link className='option'to='signin'>
+                SIGN IN
+            </Link>
 
         </div>
     </div>
