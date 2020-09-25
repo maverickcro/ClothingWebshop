bc2f872389989fd3c4fcaf99bff9f01ce3156001
maverickcro <mav3ricktv@gmail.com>
2020-09-25 15:25:26 +0200

    added checkout functionalities, increase and decrease item quantity AND clear(remove) item from checkout

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -12,3 +12,13 @@ export const addItem = item => ({
     payload: item
 });
 
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+});
+
+export const clearItem = item => ({
+    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    payload: item
+    
+});
